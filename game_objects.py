299fcc0abbb04086c5ed52d7031980ca6aaf84b2
TechTTGames <50541739+Tech-TTGames@users.noreply.github.com>
2022-05-11 10:23:19 +0200

    Added basic stuff

---
+++ game_objects.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+class Pipe(pg.sprite.Sprite):
+    def __init__(self, game):
+        super().__init__()
+        self.screen = game.screen
+        self.settings = game.settings
+        self.screen_rect = self.screen.get_rect()
+
+        self.image = pg.image.load("sprites/pipe.bmp")
+        self.rect = self.image.get_rect()
+
+class Birb(pg.sprite.Sprite):
+    def __init__(self, game):
+        super().__init__()
+        self.screen = game.screen
+        self.settings = game.settings
+        self.screen_rect = self.screen.get_rect()
+
+        self.image = pg.image.load("sprites/birb.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.midleft = self.screen_rect.midleft
+
+        self.jumping = False
--- main-game.py
+++ main-game.py
@@ -1,2 +1,15 @@
 import pygame as pg
 import game_objects
+import json
+
+class MusiBirb:
+    def __init__(self):
+        with open('config.json') as config_file:
+            self.settings = json.load(config_file)
+        self.clock = pg.time.Clock()
+        pg.init()
+        pg.display.set_caption("Flappy Music!")
+        
+        resolution = (self.settings["screen-width"],self.settings["screen-height"])
+        self.screen = pg.display.set_mode(resolution)
+        self.birb = game_objects.Birb()
